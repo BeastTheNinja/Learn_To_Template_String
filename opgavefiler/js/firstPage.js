674303bb9a6c6f2f10f474daa65bddd92072afab
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-09-02 09:28:20 +0200

    done with opgave 1 and 2

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -2,13 +2,48 @@
 
 
 // din kode her
-
-
-/* Opgave 1*/
+const myName = 'John Doe'
+const years = 15
+const besked = `Hej ${myName} du har nu arbejdet i vores virksomhed i ${years} år`
+console.log(besked)
+const message = document.getElementById('message')
+message.innerHTML = besked
+/* Opgave 2*/
 
 
 // din kode her
+const produktListe = document.getElementById('produktListe');
+const produkt = document.getElementsByClassName(
+    'produkt'
+)
+const myData=[
+            { "navn": "T-shirt",
+            "beskrivelse": "En t-shirt lavet af bomuld",
+            "pris": 99 },
+            { "navn": "Jeans",
+            "beskrivelse": "Blå denim jeans med straight fit",
+            "pris": 299
+            },
+            { "navn": "Hættetrøje",
+            "beskrivelse": "En grå hættetrøje med lynlås",
+            "pris": 199 }
+            ];
 
+const createProductList = {
+    template: ''
+}
+
+for (let i = 0; i < myData.length; i++) {
+    createProductList.template += `
+   <article class="produkt">
+        <h2>${myData[i].navn}</h2>
+        <p>${myData[i].beskrivelse}</p>
+        <p>Pris: ${myData[i].pris} kr.</p>
+   </article>
+`;
+}
+
+produktListe.innerHTML = createProductList.template;
 
 
 /* Opgave 3 - skriv videre på koden her: */
